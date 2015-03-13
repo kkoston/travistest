0961ede94c106f270fa37af075a79179e7ea8788
Chris <chris@koston.us>
2015-03-13 15:43:57 -0400

    Added some package installation

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-echo "Running tests..."
-echo "Tests finished."
+install_apache() {
+    sudo apt-get -q -y install apache2
+}
+
+install_mysql() {
+    sudo apt-get -q -y install mysql-server
+}
+
+install_wordpress() {
+    echo "Now, install WP"
+}
+
+echo "Setting stuff up..."
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update
+install_apache
+install_mysql
+install_wordpress
+
+echo "All done!"
+
 exit 0;
