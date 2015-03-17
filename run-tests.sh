cdea5471b479827728ff9d98063160435c13f7f5
Chris <chris@koston.us>
2015-03-17 11:59:51 -0400

    Updated dump added build dev

--- run-tests.sh
+++ run-tests.sh
@@ -2,32 +2,40 @@
 
 install_apache() {
     sudo apt-get -q -y install apache2
-    
+    sudo apt-get -q -y install php5-fpm    
 }
 
 install_mysql() {
     sudo apt-get -q -y install mysql-server
+    gunzip -c build-files/dbdump.sql.gz | mysql -u root 
+    
 }
 
 install_wordpress() {
     cd /var/www
-    rm -rf index.html
-    
-    svn co http://core.svn.wordpress.org/trunk/
-    mv trunk wordpress
+    sudo rm -rf index.html
+    sudo rm -rf wordpress
+    sudo svn co http://core.svn.wordpress.org/trunk/
+    sudo mv trunk wordpress
+    cd wordpress
+    sudo cp $B_DIR/build-files/wp-config.php .
     
     cd $B_DIR
 }
 
-B_DIR=`pwd`
-
 echo "Setting stuff up..."
+# dev instance clone like Travis
+if [ -d "/home/vagrant" ]; then
+    cd /home/vagrant/travistest
+fi
+
+B_DIR=`pwd`
 
 # use non-interactive package installation
 export DEBIAN_FRONTEND=noninteractive
 
 # update repos
-sudo apt-get update
+# !!!!!!!!!!!uncomment!!!!!!!!!!!!!!!uncomment!!!!!!!!!!!!!!!!!!!!uncomment!!!!!!!!!!!!!!!!!       sudo apt-get update
 
 # install components and confugure them
 install_apache
