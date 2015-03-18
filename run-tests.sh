b120f7fb765c7522462c34769777e5161ef28746
Chris <chris@koston.us>
2015-03-18 14:56:29 -0400

    Trying the VIP quickstart

--- run-tests.sh
+++ run-tests.sh
@@ -1,47 +1,11 @@
 #!/bin/sh
-
-install_apache() {
-    sudo apt-get -q -y install apache2
-    sudo apt-get -q -y install php5-fpm    
-}
-
-install_mysql() {
-    sudo apt-get -q -y install mysql-server
-    gunzip -c build-files/dbdump.sql.gz | mysql -u root 
-    
-}
-
-install_wordpress() {
-    cd /var/www
-    sudo rm -rf index.html
-    sudo rm -rf wordpress
-    sudo svn co http://core.svn.wordpress.org/trunk/
-    sudo mv trunk wordpress
-    cd wordpress
-    sudo cp $B_DIR/build-files/wp-config.php .
-    
-    cd $B_DIR
-}
-
-echo "Setting stuff up..."
-# dev instance clone like Travis
-if [ -d "/home/vagrant" ]; then
-    cd /home/vagrant/travistest
-fi
-
-B_DIR=`pwd`
-
-# use non-interactive package installation
-export DEBIAN_FRONTEND=noninteractive
-
-# update repos
-# !!!!!!!!!!!uncomment!!!!!!!!!!!!!!!uncomment!!!!!!!!!!!!!!!!!!!!uncomment!!!!!!!!!!!!!!!!!       sudo apt-get update
-
-# install components and confugure them
-install_apache
-install_mysql
-install_wordpress
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y git puppet
+mkdir /srv
+cd /srv/ && sudo git clone https://github.com/Automattic/vip-quickstart .
+sudo /srv/bin/vip-init --server --domain=localhost
 
 echo "All done!"
+
 
 exit 0;
